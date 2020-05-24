e3939ab45921b4af28cab7ad9d76fc457048dc97
cmatkhan <chase.mateusiak@gmail.com>
2020-05-23 21:17:36 -0500

    remove scaffold complete

--- remove_scaffold_from_genome_fasta.py
+++ remove_scaffold_from_genome_fasta.py
@@ -19,12 +19,13 @@
     usage: remove_scaffold_from_genome_fasta.py -g genome.fasta -f filter_file.txt
 
     written_by: Kamil S Jaron (https://bioinformatics.stackexchange.com/a/3940)
-    included_in_misc_scripts: 5/21/20 by chase.mateusiak@gmail.com
+    modified_by: chase.mateusiak@gmail.com
     dependencies: Biopython, argparse
 """
 from Bio import SeqIO
 import argparse
 import os
+import sys
 
 def main(argv):
     """
@@ -33,23 +34,50 @@ def main(argv):
     print('...parsing input')
     args = parse_args(argv)
     try:
-        if not os.path.isfile(args.genome)
+        if not os.path.isfile(args.genome):
             raise FileNotFoundError('GenomeFileNotFound')
     except FileNotFoundError:
         print('The path to the genome file is not valid. Check and try again')
     else:
         genome_fasta_path = args.genome
     try:
-        if not os.path.isfile(filter_file_path = args.filter_file
+        if not os.path.isfile(args.filter_file):
+            raise FileNotFoundError('FilterFileNotFound')
+    except FileNotFoundError:
+        print('The path to the filter file is not valid. Check and try again.')
+    else:
+        filter_file_path = args.filter_file
+    finally:
+        if not (os.path.isfile(args.genome) and os.path.isfile(args.filter_file)):
+            sys.exit()
+
+    # read filter_file_path and create a set
+    print('...reading filter_file')
+    with open(filter_file_path, 'r') as file:
+        scaffold_to_remove_set = set(line.strip() for line in file)
+
+    # create generator from fasta file
+    genome_generator = SeqIO.parse(genome_fasta_path, 'fasta')
+    # loop over genome, if the header is in the filter file
+    print('...filtering input %s for scaffolds in %s' %(genome_fasta_path, filter_file_path))
+    for seq_record in genome_generator:
+        try:
+            # seq_record.name is in scaffold_to_remove_set, pop it out of the set and continue in loop
+            scaffold_to_remove_set.remove(seq_record.name)
+        except KeyError:
+            print(seq_record.format('fasta').strip())
+    if len(scaffold_to_remove_set) != 0:
+        print('%s not found in %s' %(scaffold_to_remove_set, genome_fasta_path))
 
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(
         description="filter scaffold from genome fasta")
     parser.add_argument("-g", "--genome", required=True,
-                        help="[REQUIRED] genome in fasta format"
+                        help="[REQUIRED] genome in fasta format")
     parser.add_argument("-f", "--filter_file", required=True,
                         help="[REQUIRED] file with the headers to remove (see doc string at top of file, use head, to see example/description)")
+
     args = parser.parse_args(argv[1:])
     return args
 
