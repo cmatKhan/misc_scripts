6a39ac3d96a5a2f90f4f31b5b223c696fb13f344
cmatkhan <chase.mateusiak@gmail.com>
2020-07-15 11:07:07 -0500

    pushing to repo;

--- update_qa1_audit.py
+++ update_qa1_audit.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
+# INPUT 1: query_df
+# INPUT 2: the sheet to audit
+
 import sys
-from rnaseq_tools.QualityAssessmentObject import QualityAssessmentObject
+import pandas as pd
+from rnaseq_tools.CryptoQualityAssessmentObject import CryptoQualityAssessmentObject
 
 print('...creating quality assessment object')
-qa = QualityAssessmentObject(interactive=True)
+qa = CryptoQualityAssessmentObject(interactive=True)
+
+qa.query_df = pd.read_csv(sys.argv[1])
 
 print('...auditing sheet')
-df = qa.auditQualAssess1(sys.argv[1])
+df = qa.auditQualAssessDataFrame(sys.argv[2])
 
-print('...writing to %s' %sys.argv[1])
-df.to_csv(sys.argv[1], index=False)
+print('...writing to %s' %sys.argv[2])
+df.to_csv(sys.argv[2], index=False)
