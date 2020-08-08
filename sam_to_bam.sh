3f1cf3cd8ad5b7277eda8482239f27e546bf82a7
cmatkhan <chase.mateusiak@gmail.com>
2020-08-08 09:59:03 -0500

    adding deseq_model.R

--- sam_to_bam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# convert sam file to bam (specifically if you get the error "no SQ lines in header" when trying samtools -bS view input_sam.sam > output_bam.bam)
-# first input is genome (.fasta) second is the sam file third is the name or path of the bam output
-
-samtools view -bS -T $1 $2 > $3
