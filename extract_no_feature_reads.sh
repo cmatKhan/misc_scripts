d5b847f87e21b93368afb554a4b2bf073555d2b0
cmatkhan <chase.mateusiak@gmail.com>
2020-08-11 10:33:16 -0500

    deleting files

--- extract_no_feature_reads.sh
+++
@@ -1,7 +0,0 @@
-
-# first, remove tab from front of .sam output by htseq
-sed '/\t//' <the htseq sam> > outfile
-
-# next, paste the column onto the end of each line in the bam grep for no feature and write out
-
-samtools view <bam> | paste - b > outbam | grep no_feature > outfile
