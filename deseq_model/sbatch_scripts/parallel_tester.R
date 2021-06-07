fb41a9e8dd6355841e46ffcd3fa24693fe725e1a
cmatKhan <chase.mateusiak@gmail.com>
2021-06-07 17:08:36 -0500

    Update parallel_tester.R

--- deseq_model/sbatch_scripts/parallel_tester.R
+++ deseq_model/sbatch_scripts/parallel_tester.R
@@ -4,12 +4,12 @@ suppressMessages(library(BiocParallel))
 
 print('no parallel')
 register(SerialParam())
-print(system.time({ bplapply(1:4, function(i) Sys.sleep(5)) }))
+print(system.time({ bplapply(1:10, function(i) Sys.sleep(5)) }))
 
 print('with 4')
 register(MulticoreParam(workers=4))
-print(system.time({ bplapply(1:4, function(i) Sys.sleep(5)) }))
+print(system.time({ bplapply(1:10, function(i) Sys.sleep(5)) }))
 
 print('with 10')
-register(MulticoreParam(workers=4))
-print(system.time({ bplapply(1:4, function(i) Sys.sleep(5)) }))
+register(MulticoreParam(workers=10))
+print(system.time({ bplapply(1:10, function(i) Sys.sleep(5)) }))
